8a657e821c8a5a886d796191c109cfec5efa4b91
juanitat <s22120193@alumnos.itsur.edu.mx>
2025-09-25 12:37:55 -0600

    paso 5:  Agregue los elementos de la cuadrícula de temas.

--- app/src/main/java/com/example/mycuadricula/MainActivity.kt
+++ app/src/main/java/com/example/mycuadricula/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.mycuadricula.sampledata.DataSource
 import com.example.mycuadricula.ui.theme.MycuadriculaTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MycuadriculaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
@@ -31,17 +34,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun TopicGrid(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
         modifier = modifier
-    )
+    ) {
+        items(DataSource.topics) { topic ->
+            TopicCard(topic)
+            }
+        }
+}
+
+@Composable
+fun TopicCard(x0: Int) {
+    TODO("Not yet implemented")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MycuadriculaTheme {
-        Greeting("Android")
+
     }
 }
