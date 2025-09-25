62d10d9a0b079780537de51dc6e743694b419f27
juanitat <s22120193@alumnos.itsur.edu.mx>
2025-09-25 12:30:49 -0600

    paso 4: Agregue las imagenes y coloque el datasource en una nueva carpeta de datos

---
+++ app/src/main/java/com/example/mycuadricula/model/Topic.kt
@@ -0,0 +1,6 @@
+package com.example.mycuadricula.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic (@StringRes val name: Int, val availableCourses: Int, @DrawableRes val imageRes: Int)
